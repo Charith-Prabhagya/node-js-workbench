a876fada343244226ac6c8dc36c02a8081ddbce4
cprabhagya@gmail.com <cprabhagya@gmail.com>
2021-09-26 22:35:46 +0530

    read variable

--- basics/server.js
+++ basics/server.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var module1 = require('./module1');
 
 function onRequest(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
+    res.write(module1.myVariable);
+    module1.myFunction();
     res.end();
 }
 
